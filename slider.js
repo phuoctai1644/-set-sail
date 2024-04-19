2b5753928a6d1ae6c2148c585ac9d50c06baf2fa
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2024-04-19 22:18:59 +0700

    fix: improve ui and fix slider's config

--- slider.js
+++ slider.js
@@ -1,24 +1,25 @@
-var splide = new Splide('.splide', {
-  type: 'loop',
-  autoplay: true,
-  classes: {
-  // pagination: 'splide__pagination feedback__pagination',
-    // page: 'splide__pagination__page feedback__pagination',
-  },
-});
-
-var splide2 = new Splide('.splide2', {
-  type: 'loop',
-  autoplay: true,
-  perPage: 4,
-  gap: '24px'
-})
-
-var splide3 = new Splide('.splide3', {
-  type: 'loop',
-  autoplay: true,
-})
-
-splide.mount();
-splide2.mount();
-splide3.mount();
+document.addEventListener('DOMContentLoaded', function() {
+  const splide1 = document.querySelector('#splide1');
+  const splide2 = document.querySelector('#splide2');
+  const splide3 = document.querySelector('#splide3');
+  
+  new Splide(splide1, { type: 'loop', autoplay: true }).mount();
+  new Splide(splide2, {
+    type: 'loop',
+    autoplay: true,
+    perPage: 4,
+    gap: '24px',
+    classes: {
+      arrows: 'splide__arrows d-none'
+    }
+  }).mount();
+  new Splide(splide3, {
+    type: 'loop',
+    autoplay: true,
+    classes: {
+      pagination: 'splide__pagination feedback__pagination',
+      prev: 'splide__arrow--prev feedback__arrow-prev',
+      next: 'splide__arrow--next feedback__arrow-next'
+    }
+  }).mount();
+} );
